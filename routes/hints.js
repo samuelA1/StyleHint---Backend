005e51238721c8abc6fc79df02f02241e60f2033
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-10 09:29:15 -0400

    added virtual to hint schema

--- routes/hints.js
+++ routes/hints.js
@@ -25,7 +25,8 @@ router.post('/get-hints',checkJwt, (req, res) => {
                   {season: req.body.season}, {occasion: req.body.occasion}]})
             .limit(perPage)
             .skip(page * perPage)
-            .select(['_id', 'url', 'overview', 'recommendations', 'alternatives', 'do', 'dont'])
+            .select(['url'])
+            // .select(['_id','averageRating', 'url', 'overview', 'recommendations', 'alternatives', 'do', 'dont'])
             .sort({averageRating: -1})
             .exec((err, hints) => {
                 callback(err, hints)
