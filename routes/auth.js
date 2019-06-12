ede144d66ef679b4c48356134aa03be3536c5454
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-12 18:18:00 -0400

    prone

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const async = require('async');
 //login route
 router.post('/login', (req, res) => {
     User.findOne({email: req.body.email})
-    .select(['-password', '-friends'])
+    .select(['-password'])
     .exec((err, userExist) => {
         if (err) return err;
 
