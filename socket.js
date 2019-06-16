013a1856c34ffd04d5ba8bf085a1c598388266ad
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-16 09:49:57 -0400

    prone

--- socket.js
+++ socket.js
@@ -1,12 +1,12 @@
 module.exports = function (io) {
     io.on('connection', (socket) => {
-        socket.on('send-hint', (data) => {
+        socket.on('send', (data) => {
             // console.log(data)
             // data.friends.forEach(friend => {
             //     friend.join('share');
             //     socket.to('share').emit('share-hint', "let's play a game");
             // });
-            io.emit('share-hint', "let's play a game")
+            io.emit('share', "let's play a game")
         })
     });
 }
