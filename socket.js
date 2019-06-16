9d75dfa7525b292b8e471e4af330a483ad2d5430
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-16 08:52:55 -0400

    prone

--- socket.js
+++ socket.js
@@ -1,10 +1,10 @@
-// module.exports = function (io) {
-//     io.on('connection', (socket) => {
-//         socket.on('send-hint', (data) => {
-//             data.friends.forEach(friend => {
-//                 friend.join('share');
-//                 socket.to('share').emit('share-hint', "let's play a game");
-//             });
-//         })
-//     });
-// }
+module.exports = function (io) {
+    io.on('connection', (socket) => {
+        // socket.on('send-hint', (data) => {
+        //     data.friends.forEach(friend => {
+        //         friend.join('share');
+        //         socket.to('share').emit('share-hint', "let's play a game");
+        //     });
+        // })
+    });
+}
