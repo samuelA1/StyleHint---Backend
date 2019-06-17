cc0a9c5f4a0bd96f6ff57c57650358f3171488b6
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-17 04:09:09 -0400

    prone

--- routes/tips.js
+++ routes/tips.js
@@ -61,24 +61,18 @@ router.get('/get-tips', checkJwt, (req, res) => {
             if (user['tips'] !== null) {
                 for (let i = 0; i < user['tips'].length; i++) {
                     const tip = user['tips'][i];
-                    Tip.findById(tip)
-                    // .select(['owner', 'imageUrl', 'hintId', '_id'])
-                    .populate('owner')
-                    .exec((err, tipsGotten) => {
+                    Tip.findById(tip, (err, tipsGotten) => {
                         if (err) return err;
     
                         sharedTips.push(tipsGotten);
-                    })
+                    });
                 }
             }
 
             if (user['myTips'] !== null) {
                 for (let i = 0; i < user['myTips'].length; i++) {
                     const tip = user['myTips'][i];
-                    Tip.findById(tip)
-                    // .select(['owner', 'imageUrl', 'hintId', '_id'])
-                    .populate('owner')
-                    .exec((err, tipsGotten) => {
+                    Tip.findById(tip, (err, tipsGotten) => {
                         if (err) return err;
     
                         myTips.push(tipsGotten);
@@ -87,27 +81,74 @@ router.get('/get-tips', checkJwt, (req, res) => {
                             myTips: myTips,
                             sharedTips: sharedTips
                         })
-                    })
+                    });
                 }
             }
         }
     ])
 });
 
-//get a single tip
-router.get('/get-single-tip/:id', checkJwt, (req, res) => {
-    Tip.findById(req.params.id)
-    .select(['-usersToSee'])
-    .populate('owner')
-    .exec( (err, tipsGotten) => {
-        if (err) return err;
+// router.get('/get-single-tip/:id', checkJwt, (req, res) => {
+//     Tip.findById(req.params.id)
+//     .select(['-usersToSee'])
+//     .populate('owner')
+//     .exec( (err, tipsGotten) => {
+//         if (err) return err;
 
-        res.json({
-            success: true,
-            tip: tipsGotten
-        })
-    })
-});
+//         res.json({
+//             success: true,
+//             tip: tipsGotten
+//         })
+//     })
+// });
+
+// router.get('/get-tips', checkJwt, (req, res) => {
+//     var sharedTips = [];
+//     var myTips = [];
+//     async.waterfall([
+//         function (callback) {
+//             User.findById(req.decoded.user._id, (err, user) => {
+//                 if (err) return err;
+
+//                 callback(err, user)
+//             });
+//         },
+//         function (user) {
+//             if (user['tips'] !== null) {
+//                 for (let i = 0; i < user['tips'].length; i++) {
+//                     const tip = user['tips'][i];
+//                     Tip.findById(tip)
+//                     .select(['owner', 'imageUrl', 'hintId', '_id'])
+//                     .populate('owner')
+//                     .exec((err, tipsGotten) => {
+//                         if (err) return err;
+    
+//                         sharedTips.push(tipsGotten);
+//                     })
+//                 }
+//             }
+
+//             if (user['myTips'] !== null) {
+//                 for (let i = 0; i < user['myTips'].length; i++) {
+//                     const tip = user['myTips'][i];
+//                     Tip.findById(tip)
+//                     .select(['owner', 'imageUrl', 'hintId', '_id'])
+//                     .populate('owner')
+//                     .exec((err, tipsGotten) => {
+//                         if (err) return err;
+    
+//                         myTips.push(tipsGotten);
+//                         res.json({
+//                             success: true,
+//                             myTips: myTips,
+//                             sharedTips: sharedTips
+//                         })
+//                     })
+//                 }
+//             }
+//         }
+//     ])
+// });
 
 //add comment to tip
 router.post('/add-comment/:id', checkJwt, (req, res) => {
