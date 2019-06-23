655f39096916a2b2b7f257371f314e31615106ed
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-23 14:19:39 -0400

    prone

--- routes/tips.js
+++ routes/tips.js
@@ -124,10 +124,7 @@ router.post('/seenBy/:id', checkJwt, (req, res) => {
         if (tip.seenBy.length !== 0) {
             tip.seenBy.forEach(tipGot => {
                 if (tipGot == req.decoded.user._id) {
-                    res.json({
-                        success: true,
-                        message: 'tip seen'
-                    });
+                    
                 } else {
                     tip.seenBy.push(req.decoded.user._id);
                     tip.save();
