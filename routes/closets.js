a23781ede31b51523228074cd96ee42bc00bc3cd
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-25 03:05:18 -0400

    prone

--- routes/closets.js
+++ routes/closets.js
@@ -88,7 +88,6 @@ router.post('/remove-closet', checkJwt, (req, res) => {
 router.get('/my-closet', checkJwt, (req, res) => {
     Closet.findOne({owner: req.decoded.user._id})
     .populate('collections.hints')
-    .select(['collections.hints._id', 'collections.hints.url'])
     .exec( (err, closetGot) => {
         if (err) return err;
 
