33eff0d2cd54b456873cec7677b3a068087887eb
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-25 02:45:05 -0400

    created remove from closet

--- routes/closets.js
+++ routes/closets.js
@@ -4,6 +4,7 @@ const Hint = require('../models/hint');
 const checkJwt = require('../middleware/check-jwt');
 const async = require('async');
 
+//add hint to closet
 router.post('/add-closet', checkJwt, (req, res) => {
     async.waterfall([
         function (callback) {
@@ -21,7 +22,7 @@ router.post('/add-closet', checkJwt, (req, res) => {
                     let closet = new Closet();
                     closet.owner = req.decoded.user._id;
                     closet.collections.push({
-                        name: 'all',
+                        name: req.body.collectionName,
                         hints: [req.body.hintId]
                     });
                     hint.likedBy.push(req.decoded.user._id);
@@ -49,5 +50,53 @@ router.post('/add-closet', checkJwt, (req, res) => {
         }
     ])
 });
+
+//remove hint from closet
+router.post('/remove-closet', checkJwt, (req, res) => {
+    async.waterfall([
+        function (callback) {
+            Hint.findById(req.body.hintId, (err, hint) => {
+                if (err) return err;
+
+                callback(err, hint);
+            });
+        },
+        function (hint) {
+            Closet.findOne({owner: req.decoded.user._id}, (err, closetGot) => {
+                if (err) return err;
+        
+                
+                let closetAdd = closetGot.collections.find(collection => 
+                    collection['name'] == req.body.collectionName);
+                const toRemove = closetAdd.hints.indexOf(req.body.hintId)
+                closetAdd.hints.splice(toRemove, 1);
+                const hintRemove = hint.likedBy.indexOf(req.decoded.user._id)
+                hint.likedBy.splice(hintRemove, 1);
+
+                hint.save();
+                closetGot.save();
+                res.json({
+                    success: true,
+                    message: 'Hint removed from closet'
+                })
+            })
+        }
+    ])
+});
+
+//get closet
+router.get('/my-closet', checkJwt, (req, res) => {
+    Closet.findOne({owner: req.decoded.user._id})
+    .populate('collections.hints')
+    .exec( (err, closetGot) => {
+        if (err) return err;
+
+        res.json({
+            success: true,
+            closet: closetGot
+        })
+    })
+});
+
 
 module.exports = router;
