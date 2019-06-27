a94dab659cfda373c8b711b39aded3c4c3177adc
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-27 00:18:56 -0400

    prone

--- routes/tips.js
+++ routes/tips.js
@@ -271,45 +271,46 @@ router.post('/auto-delete', checkJwt, (req, res) => {
         function (user, totalComments) {
             if (user.myTips.length !== 0) {
                 user.myTips.forEach(myTip => {
-                    // Tip.findById(myTip, (err, tip) => {
-                    //     if (err) return err;
+                    Tip.findById(myTip, (err, tip) => {
+                        if (err) return err;
         
-                    //     if (tip == null) {
-                    //         User.find({tips: myTip}, (err, userWithMyTips) => {
-                    //             if (err) return err;
+                        if (tip == null) {
+                            User.find({tips: myTip}, (err, userWithMyTips) => {
+                                if (err) return err;
         
-                    //             for (let i = 0; i < userWithMyTips.length; i++) {
-                    //                 const tipToRemove = userWithMyTips[i].tips.indexOf(myTip)
-                    //                 userWithMyTips[i].tips.splice(tipToRemove, 1);
-                    //                 if (userWithMyTips[i].notifications == -1) {
-                    //                     userWithMyTips[i].notifications = 0;
-                    //                 } else {
-                    //                     userWithMyTips[i].notifications = userWithMyTips[i].notifications - 1;
-                    //                 }
-                    //                 userWithMyTips[i].save();
-                    //             }
-                    //         });
+                                for (let i = 0; i < userWithMyTips.length; i++) {
+                                    const tipToRemove = userWithMyTips[i].tips.indexOf(myTip)
+                                    userWithMyTips[i].tips.splice(tipToRemove, 1);
+                                    if (userWithMyTips[i].notifications == -1) {
+                                        userWithMyTips[i].notifications = 0;
+                                    } else {
+                                        userWithMyTips[i].notifications = userWithMyTips[i].notifications - 1;
+                                    }
+                                    userWithMyTips[i].save();
+                                }
+                            });
         
-                    //         Notification.deleteMany({route: myTip}, (err) => {
-                    //             if (err) return err;
-                    //         })
+                            Notification.deleteMany({route: myTip}, (err) => {
+                                if (err) return err;
+                            })
                 
-                    //         const toRemove = user.myTips.indexOf(myTip)
-                    //         user.myTips.splice(toRemove, 1);
-                    //         if (user.notifications == -1) {
-                    //             user.notifications = 0;
-                    //         } else {
-                    //             user.notifications = user.notifications - totalComments;
-                    //         }
+                            const toRemove = user.myTips.indexOf(myTip)
+                            user.myTips.splice(toRemove, 1);
+                            if (user.notifications == -1) {
+                                user.notifications = 0;
+                            } else {
+                                user.notifications = user.notifications - totalComments;
+                            }
                            
-                    //     }
-                    // })
+                        }
+                    })
                 });
-                user.save();
-                res.json({
-                    success: true
-                });
+                
             } 
+            user.save();
+            res.json({
+                success: true
+            });
         }
     ])
 });
