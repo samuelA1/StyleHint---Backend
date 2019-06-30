2edc1c3d15243087934453ac2b514a558294904d
Samuel Essim <arreyessimbate@hotmail.com>
2019-06-30 03:31:13 -0400

    prone

--- routes/closets.js
+++ routes/closets.js
@@ -191,7 +191,7 @@ router.post('/delete-collection', checkJwt, (req, res) => {
 
                 let flash = []
                 closetGot.collections.forEach(collect => {
-                    flash.push(collect.hints.some(id => id == hintId))
+                    flash.push(collect.hints.some(id => id == hint._id))
                 });
                 if (flash.includes(true)) {
                     closetGot.save();
