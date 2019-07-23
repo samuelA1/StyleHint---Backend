8282091b08621a347b25a91f782e23c511f88c61
Samuel Essim <arreyessimbate@hotmail.com>
2019-07-23 01:35:20 -0400

    prone

--- routes/auth.js
+++ routes/auth.js
@@ -111,28 +111,27 @@ router.post('/register', (req, res) => {
                             <p>-- The StyleHint Team.</p>
                         </div>
                         `
-                         // create reusable transporter object using the default SMTP transport
                         let transporter = nodemailer.createTransport({
                             host: "smtp.office365.com",
                             port: 587,
                             secure: false, // true for 465, false for other ports
                             auth: {
-                            user: 'no-reply@thestylehint.com', // generated ethereal user
-                            pass: 'sneakers36.' // generated ethereal password
+                              user: 'no-reply@thestylehint.com', // generated ethereal user
+                              pass: 'sneakers36.' // generated ethereal password
                             }
-                        });
-
-                        // send mail with defined transport object
-                        let info = transporter.sendMail({
-                            from: '"StyleHint Inc" <no-reply@thestylehint.com>', // sender address
+                          });
+                        
+                          // send mail with defined transport object
+                          let info = await transporter.sendMail({
+                            from: '"StyleHint" <no-reply@thestylehint.com>', // sender address
                             to: `${req.body.email}`, // list of receivers
                             subject: "Welcome to StyleHint", // Subject line
                             text: "Hello world?", // plain text body
                             html: output // html body
-                        });
-
-                        console.log("Message sent: %s", info.messageId);
-                        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                          });
+                        
+                          console.log("Message sent: %s", info.messageId);
+                          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
                         res.json({
                             success: true,
