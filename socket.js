c61ef3fa0951c388b44d54c7e9a99ba4971c02b2
Samuel Essim <arreyessimbate@hotmail.com>
2019-07-26 06:17:03 -0400

    prone

--- socket.js
+++ socket.js
@@ -1,7 +1,8 @@
 module.exports = function (io) {
     io.on('connection', (socket) => {
         //when a user loggs in
-        io.emit('loggedIn', '')
+        var total = io.engine.clientsCount;
+        io.emit('loggedIn', total)
 
         //for sharing hints
         socket.on('send', (data) => {
