b618bce8ccf9506d11274a13b431447c114c12f0
Samuel Essim <arreyessimbate@hotmail.com>
2019-07-30 19:13:11 -0400

    prone

--- routes/admin.js
+++ routes/admin.js
@@ -72,34 +72,34 @@ router.post('/update-statistics', checkJwt, (req, res) => {
         $gte: new Date(new Date().setDate(new Date().getDate()-1))}}, (err, stats) => {
         if (err) return err;
         
-        console.log(stats);
-        res.json({success: true})
-        // if (stats.length == null) {
-        //     let statistic = new Statistics();
-        //     statistic.currentlyActiveUsers++
-        //     statistic.dailyUsers++
-        //     statistic.save();
-        //     res.json({
-        //         success: true
-        //     })
-        // } else {
-        //     if (req.body.action == 'add') {
-        //         stats.currentlyActiveUsers++
-        //         stats.dailyUsers++
-        //         stats.save();
-        //         res.json({
-        //             success: true
-        //         })
-        //     } else if(req.body.action == 'subtract') {
-        //         stats.currentlyActiveUsers--
-        //         stats.dailyUsers--
-        //         stats.save();
-        //         res.json({
-        //             success: true
-        //         })
-        //     }
+        // console.log(stats);
+        // res.json({success: true})
+        if (stats == null) {
+            let statistic = new Statistics();
+            statistic.currentlyActiveUsers++
+            statistic.dailyUsers++
+            statistic.save();
+            res.json({
+                success: true
+            })
+        } else {
+            if (req.body.action == 'add') {
+                stats.currentlyActiveUsers++
+                stats.dailyUsers++
+                stats.save();
+                res.json({
+                    success: true
+                })
+            } else if(req.body.action == 'subtract') {
+                stats.currentlyActiveUsers--
+                stats.dailyUsers--
+                stats.save();
+                res.json({
+                    success: true
+                })
+            }
            
-        // }
+        }
     });
 });
 
