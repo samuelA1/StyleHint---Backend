5c41a875f11d65c7dadeab1bdcb03913469be86d
Samuel Essim <arreyessimbate@hotmail.com>
2019-08-01 07:47:29 -0400

    prone

--- socket.js
+++ socket.js
@@ -8,7 +8,7 @@ module.exports = function (io) {
         //when user loggs out
         socket.on('disconnect', () => {
             io.emit('loggedOut',  {});
-            console.log(Object.keys(io.sockets.connected).length)        
+            console.log(io.sockets.server.engine.clientsCount)        
         });
 
         //for sharing hints
