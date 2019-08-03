60811432d422e4606f91e7c364d103f9472c3df3
Samuel Essim <arreyessimbate@hotmail.com>
2019-08-03 18:40:33 -0400

    prone

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ module.exports = function (io) {
         //logged in users
         socket.on('logIn', (data) => {
             io.emit('loggedIn', {activeUsers: io.sockets.server.engine.clientsCount});
-            console.log(io.sockets.sockets);
+            console.log(io.sockets.sockets.connected);
         })
 
         //when user loggs out
