9785480bdfbe22f56f954b76dce2d6535d5ce26f
Samuel Essim <arreyessimbate@hotmail.com>
2019-08-14 04:26:29 -0400

    prone

--- routes/admin.js
+++ routes/admin.js
@@ -106,17 +106,17 @@ router.get('/all-users', isAdmin, (req, res) => {
 
 //sort users
 router.post('/sort-users', isAdmin, (req, res) => {
-User.find({gender : {$regex : /.*son.*/}})
-        .sort({username: 1})
-        .exec((err, users) => {
-            if (err) return err;
+    User.find({gender : {$regex : /.*male.*/}})
+            .sort({username: 1})
+            .exec((err, users) => {
+                if (err) return err;
 
-            console.log(users)
-            res.json({
-                success: true,
-                users: users
-            })
-        });
+                console.log(users)
+                res.json({
+                    success: true,
+                    users: users
+                })
+    });
 });
 
 //get single users
