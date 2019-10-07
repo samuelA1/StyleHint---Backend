ce1b1940f3ca3d90fe41584c55daffd7cf2d838d
Samuel Essim <arreyessimbate@hotmail.com>
2019-10-07 15:14:24 -0400

    prone

--- routes/designer.js
+++ routes/designer.js
@@ -86,33 +86,33 @@ router.post('/add-product', isDesigner, (req, res) => {
         Alert.find({}, (err, alert) => {
             if (err) return err;
 
-            if (alert == null) {
-                // let alert = new Alert();
-                // alert.numberOfAlerts = 1
-                // alert.save();
-                // notification.fromUsername = 'admin';
-                // notification.typeOf = 'review';
-                // notification.message = 'A designer just submitted a product for review.';
-                // notification.route = product._id;
-                // notification.save();
-                // res.json({
-                //     success: true,
-                //     message: 'Product successfully added.'
-                // });
-                console.log(alert);
-            } else {
-                alert[0].numberOfAlerts++
-                alert[0].save();
-                notification.fromUsername = 'admin';
-                notification.typeOf = 'review';
-                notification.message = 'A designer just submitted a product for review.';
-                notification.route = product._id;
-                notification.save();
-                res.json({
-                    success: true,
-                    message: 'Product successfully added.'
-                });
-            }
+            // if (alert == null) {
+            //     let alert = new Alert();
+            //     alert.numberOfAlerts = 1
+            //     alert.save();
+            //     notification.fromUsername = 'admin';
+            //     notification.typeOf = 'review';
+            //     notification.message = 'A designer just submitted a product for review.';
+            //     notification.route = product._id;
+            //     notification.save();
+            //     res.json({
+            //         success: true,
+            //         message: 'Product successfully added.'
+            //     });
+            // } else {
+            //     alert[0].numberOfAlerts++
+            //     alert[0].save();
+            //     notification.fromUsername = 'admin';
+            //     notification.typeOf = 'review';
+            //     notification.message = 'A designer just submitted a product for review.';
+            //     notification.route = product._id;
+            //     notification.save();
+            //     res.json({
+            //         success: true,
+            //         message: 'Product successfully added.'
+            //     });
+            // }
+            console.log(alert);
         })
 
        
