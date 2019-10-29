6cba0112201f382de445c2cdd04a075643dd5087
Samuel Essim <arreyessimbate@hotmail.com>
2019-10-29 16:03:08 -0400

    prone

--- routes/business.js
+++ routes/business.js
@@ -109,7 +109,7 @@ router.get('/unchosen-designers', checkJwt, (req, res) => {
                     // if (user.designers.some(d => d == '"' + des._id + '"')) {
                     //     filteredDesigners.push(des);
                     // }
-                    console.log(user.designers.some(d => d = des._id));
+                    console.log(user.designers.some(d => d == des._id));
                 });
                 
     
