531238c416660d43c65a3439d556c2066fb8c225
Samuel Essim <arreyessimbate@hotmail.com>
2019-11-06 13:30:04 -0500

    prone

--- routes/business.js
+++ routes/business.js
@@ -350,6 +350,29 @@ router.post('/clear-cart', checkJwt, (req, res) => {
 
 //charge or payments
 router.post('/pay', checkJwt, (req, res) => {
+    Math.round10 = function(value, exp) {
+        return decimalAdjust('round', value, exp);
+    };
+
+    function decimalAdjust(type, value, exp) {
+        // If the exp is undefined or zero...
+        if (typeof exp === 'undefined' || +exp === 0) {
+          return Math[type](value);
+        }
+        value = +value;
+        exp = +exp;
+        // If the value is not a number or the exp is not an integer...
+        if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+          return NaN;
+        }
+        // Shift
+        value = value.toString().split('e');
+        value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+        // Shift back
+        value = value.toString().split('e');
+        return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+    }
+    
     let order = new Order();
     let notification = new Notification();
 
@@ -381,7 +404,7 @@ router.post('/pay', checkJwt, (req, res) => {
                     });
                     }).then(function(charge) {
                         if (charge.payment_method_details.card.country == "US") {
-                            order.fees = Math.ceil(((2.9 * req.body.amount) / 100) + 0.30);
+                            order.fees = Math.round10(((2.9 * req.body.amount) / 100) + 0.30, -2);
                         } else {
                             order.fees = 1
                         }
