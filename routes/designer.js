9ab3a2d757860e5a8ee03f69e143ef051724e6d2
Samuel Essim <samuele_con@c-hit.com>
2019-12-23 11:13:42 -0500

    prone

--- routes/designer.js
+++ routes/designer.js
@@ -320,16 +320,19 @@ router.get('/daily-finances', isDesigner, (req, res) => {
                 for (let i = 0; i < orders.length; i++) {
                     totalSold += orders[i].designerReceived;
                     totalQuantity += orders[i].products.length;
-                    res.json({
-                        success: true,
-                        totalOrders: totalOrders,
-                        totalSold: totalSold,
-                        totalQuantity: totalQuantity,
-                        orders: orders
-        
-                    })
+                //    if (i === orders.length) {
+                    
+                //    }
                     
                 }
+                res.json({
+                    success: true,
+                    totalOrders: totalOrders,
+                    totalSold: totalSold,
+                    totalQuantity: totalQuantity,
+                    orders: orders
+    
+                })
             } else {
                 res.json({
                     success: true,
